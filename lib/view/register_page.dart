07bd1403308c09f9c6152e1b82bc89784ddeed48
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2023-12-23 21:14:44 +0700

    nambahin const

--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -1,12 +1,10 @@
-import 'dart:html';
-
 import 'package:base_todolist/view/login_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
-  RegisterPage({super.key});
+  const RegisterPage({super.key});
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
